5b35e4b21071f506c3b88df1392676dec458e7e3
Hannah Sun <hannah@Hannahs-MBP.fios-router.home>
2020-04-13 19:36:20 -0400

    first command

---
+++ src/components/footerComponent/footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer>
+      <div className="section-wrapper">
+        this is my footer
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/components/pages/thumbs.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Thumbs() {
+  return (
+    <div className="thumbs">
+        <div className="section-wrapper">
+        project thumbnails go here (Homepage)
+      </div>
+    </div>
+  );
+}
+
+export default Thumbs;
