e3fffd49d45e8d392b6d2e164c4b2dddf3a01598
Hannah Sun <sunhanqing@hotmail.com>
2020-04-18 10:24:59 -0400

    updated files

---
+++ src/components/pages/about.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function About() {
+  return (
+    <div className="about">
+        <div className="section-wrapper">
+        my about section
+      </div>
+    </div>
+  );
+}
+
+export default About;
