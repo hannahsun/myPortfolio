8c6ff5bf1d47bf72eaa78a13cc5668613d818b1b
Hannah Sun <sunhanqing@hotmail.com>
2020-04-19 09:11:32 -0400

    new files

---
+++ src/components/projectPages/motionReel.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Grid from '@material-ui/core/Grid';
+import ResponsivePlayer from '../responsivePlayer.js';
+import "../../style/projectPage.css"
+
+function motionReel() {
+  return (
+    <div className="projectPage">
+        <div className="section-wrapper">
+           <div className="project-details-wrapper">
+                <h5>project info</h5>
+                <div className="project-details">
+                  <div className="description">
+                      Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+                  </div>
+                  <div>2019</div>
+                  <div>After Effects Cinema4D Illustrator Photoshop</div>
+                </div>
+            </div>
+           <ResponsivePlayer url="https://vimeo.com/378175023"/>
+      </div>
+    </div>
+  );
+}
+
+export default motionReel;
