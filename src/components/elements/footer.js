37795b654bcb58f0d2d9207aa21ff67fda078aff
Hannah Sun <sunhanqing@hotmail.com>
2020-05-06 21:50:31 -0400

    minor color change

--- src/components/elements/footer.js
+++ src/components/elements/footer.js
@@ -7,7 +7,7 @@ function Footer() {
       <div className="section-wrapper">
          <Grid container spacing={2}> 
               <Grid item xs={12} sm={3} md={2} xl={2}>
-                  <h5 className="light">Social</h5>
+                  <h5 className="lighttext">Social</h5>
                   <div className="spacer spacer-s"></div>
                   <ul className="hello">
                     <li><a className="select" target="_blank" href="https://www.linkedin.com/in/hannah-hanqing-sun-0b3248b4/">Linkedin</a> </li>
@@ -15,14 +15,14 @@ function Footer() {
                   </ul>
               </Grid>
               <Grid item xs={12} sm={5} md={7} xl={7}>
-                  <h5 className="light">Contact</h5>
+                  <h5 className="lighttext">Contact</h5>
                   <div className="spacer spacer-s"></div>
                   <ul>
                     <li><a className="select" href="mailto:sunhanqing@hotmail.com">sunhanqing@hotmail.com</a></li>
                   </ul>
               </Grid>
               <Grid item xs={12} sm={3} md={3} xl={3}>
-                    <h5 className="light">Copyright</h5>
+                    <h5 className="lighttext">Copyright</h5>
                     <div className="spacer spacer-s"></div>
                     <ul>
                     <li>© 2020 Hanqing Sun </li>
--- src/components/elements/navigation.js
+++ src/components/elements/navigation.js
@@ -14,7 +14,8 @@ function Navigation() {
           <nav>
           {/* site logo */}
           <div className="siteLogo"><Link to="/">
-                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 218.92 218.92"><defs></defs><path d="M174,174h-49v-40H94v40H45V45h49v40h31V45h49V174z M134,165h31V54h-31v40H85V54H54v111h31v-40h49V165zM218.9,218.9H0V0h218.9V218.9z M7.4,211.5h204.1V7.4H7.4V211.5z"/>
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 218.9 218.9" ><defs></defs>
+                  <path d="M174,174h-49v-40H94v40H45V45h49v40h31V45h49V174z M134,165h31V54h-31v40H85V54H54v111h31v-40h49V165zM218.9,218.9H0V0h218.9V218.9z M7.4,211.5h204.1V7.4H7.4V211.5z"/>
                   </svg>
             </Link></div>
 
