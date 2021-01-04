7495737da339ea6ebfbf3e2841b48070ff90cc87
Hannah Sun <hannahsun@hannahs-imac.myfiosgateway.com>
2021-01-04 02:00:09 -0500

    styling change for video player

--- src/components/elements/responsivePlayer.js
+++ src/components/elements/responsivePlayer.js
@@ -6,12 +6,14 @@ import "../../style/responsive-player.css"
 const ResponsivePlayer = ({ url }) => {
     return(
     <div className='player-wrapper'>
-        <ReactPlayer   
-            className = "react-player"
-            url= {url}
-            width= "100%"
-            height= "100%"
-        />
+        <div className='player-subwrapper'>
+                <ReactPlayer   
+                    className = "react-player"
+                    url= {url}
+                    width= "100%"
+                    height= "100%"
+                />
+        </div>
     </div>
     )
 }
