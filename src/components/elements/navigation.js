1ba93ff2b0dc945fe01e2214e4b73b5f5ae4aae9
Hannah Sun <hannahsun@hannahs-imac.myfiosgateway.com>
2021-01-14 18:19:59 -0500

    final change

--- src/components/elements/navigation.js
+++ src/components/elements/navigation.js
@@ -14,7 +14,7 @@ function Navigation() {
           <nav>
           {/* site logo */}
           <div className="siteLogo"><Link to="/">
-                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 45" ><defs></defs>
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50" ><defs></defs>
                   <path class="st0" d="M10.7,37.7H4.3l9.4-25.9h6.4L10.7,37.7z M45.6,11.8h-6.4l-9.4,25.9h6.4L45.6,11.8z M29.2,21.9l3.7-10.1h-6.4
 	l-3.7,10.1H29.2z M27.2,27.3h-6.4l-3.8,10.4h6.4L27.2,27.3z"/>
                   </svg>
