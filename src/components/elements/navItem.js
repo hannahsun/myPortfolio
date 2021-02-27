8664d1c20b18e1fb6e343ddef7abb416395b3d84
Hannah Sun <hannahsun@hannahs-imac.myfiosgateway.com>
2021-02-27 15:41:50 -0500

    play nav added

--- src/components/elements/navItem.js
+++ src/components/elements/navItem.js
@@ -4,12 +4,15 @@ import {Link, NavLink} from 'react-router-dom';
 const NavItem = props => (
     <ul className="menu">
               <li className="menu-item"><NavLink to="/" exact activeClassName="nav--active" >
-                  Motion Design
-                </NavLink></li>
-              <li className="menu-item"><NavLink to="/graphic" activeClassName="nav--active">Graphic Design</NavLink></li>
+                  Motion Design</NavLink></li>
+              <li className="menu-item"><NavLink to="/graphic" activeClassName="nav--active">
+                  Graphic Design</NavLink></li>
               <li className="menu-item">
-                  <NavLink to="/about" activeClassName="nav--active">About</NavLink>
-              </li>
+                  <NavLink to="/play" activeClassName="nav--active">
+                  Play</NavLink></li>
+              <li className="menu-item">
+                  <NavLink to="/about" activeClassName="nav--active">
+                  About</NavLink></li>
      </ul>
 );
 
