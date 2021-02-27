72ff3ed29f9d434b2c576fd83210a22cb03c2b87
Hannah Sun <hannahsun@hannahs-imac.myfiosgateway.com>
2021-02-27 15:37:06 -0500

    play page added

--- src/components/pages/play.js
+++ src/components/pages/play.js
@@ -1,22 +1,71 @@
 import React from 'react';
 import Gallery from 'react-photo-gallery'
 
+//image links
+import chips from '../../assets/img/play/chips.gif';
+import fan from '../../assets/img/play/fan-1.png';
+import plasticWrap from '../../assets/img/play/cloth-1.png';
+import everydayJob from '../../assets/img/play/everydayJob.gif';
+import blob from '../../assets/img/play/blob.jpg';
+import eraser from '../../assets/img/play/eraser.gif';
+import basketball from '../../assets/img/play/basketball.gif';
+import fbnews from '../../assets/img/play/fbnews.jpg';
+import legion from '../../assets/img/play/legion.png';
+import notification from '../../assets/img/play/notification.gif';
+
 const photos = [
+
         {
-          src: 'https://c2.staticflickr.com/9/8817/28973449265_07e3aa5d2e_b.jpg',
-          width: 4,
-          height: 3
+        src: eraser,
+        width: 1,
+        height: 1
+         },
+        {
+        src: fan,
+        width: 2,
+        height: 2
         },
         {
-          src: 'https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_b.jpg',
-          width: 1,
-          height: 1
+        src: plasticWrap,
+        width: 1,
+        height: 1
         },
         {
-          src: 'https://c4.staticflickr.com/9/8887/28897124891_98c4fdd82b_b.jpg',
-          width: 1,
-          height: 1
-        }
+        src: chips,
+        width: 2,
+        height: 2
+        },
+        {
+        src:basketball,
+        width: 1,
+        height: 1
+        },
+        {
+        src:legion,
+        width: 1,
+        height: 1
+        },
+        {
+        src:fbnews,
+        width: 1,
+        height: 1
+        },
+        {
+        src: everydayJob,
+        width: 1,
+        height: 1
+        },
+        {
+        src: blob,
+        width: 1,
+        height: 1
+        },   
+        {
+        src: notification,
+        width: 1,
+        height: 1
+        },     
+        
       ];
 
 
@@ -24,6 +73,6 @@ const photos = [
 export default function Play() {
         return (
                 <div>Hello world</div>,
-                <Gallery photos={photos} />
+                <Gallery photos={photos} direction={"column"}/>
         );
       }
