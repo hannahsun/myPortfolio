b879462485945ed7d7ffb79eaa5e48bf3d479ea3
Hannah Sun <hannahsun@hannahs-imac.myfiosgateway.com>
2021-04-29 23:22:59 -0400

    test viability of webpack url loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,24 @@
 module.exports = {
 
+    module: {
+        rules: [
+            {
+                test: /\.(png|jpe?g|gif|svg)(\?\S*)?$/,
+                use: {
+                  loader: 'url-loader',
+                  options: {
+                    limit: 2048,
+                    publicPath: '',
+                    publicStylePath: '../',
+                    fallback: 'react-url-loader',
+                    name: 'images/[name].[ext]?[hash]'
+                    }
+                }
+        //   { test: /\.ts$/, use: 'ts-loader' },
+            }
+        ],
+      },
+    
     //entry point
     entry:'./src/App.js',
 
@@ -7,4 +26,5 @@ module.exports = {
         path:'dist',
         filename:'bundle.js'
     }
+
 }
