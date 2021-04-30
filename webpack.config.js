79287505e74094fb710a1400094051447838997d
Hannah Sun <hannahsun@hannahs-imac.myfiosgateway.com>
2021-04-30 10:45:57 -0400

    2nd try with webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -2,27 +2,29 @@ module.exports = {
 
     //entry point
     entry:'./src/App.js',
+    
+    module:{
+        rules:[
+            {
+                test:/\.css$/,
+                use:[
+                    "css-loader"
+                ]
+            },
+            {
+                test:/\.(png|jpg|gif)$/,
+                use:[
+                    "file-loader"
+                ]
+            }
+        ],
+    }
 
-    rules:[
-        {
-            test:/\.css$/,
-            use:[
-                "css-loader"
-            ]
-        },
-        {
-            test:/\.(png|jpg|gif)$/,
-            use:[
-                "file-loader"
-            ]
-        }
-    ],
 
-
-    output:{
-        path:'dist',
-        filename:'bundle.js'
-    },
+    // output:{
+    //     path:'dist',
+    //     filename:'bundle.js'
+    // },
     
 
 } 
